9a8735f4768756a1a852e64264ec5c312e5a6b03
Brandon Burnett <gentoolicious@gmail.com>
2012-04-25 18:24:43 -0700

    fixed syntax error in initial upload

--- aggregator.py
+++ aggregator.py
@@ -1,13 +1,13 @@
 import web
 
-urls = {
-	'/', 'index'
-}
+urls = (
+    '/', 'index'
+)
 
 class index:
-	deg GET(self):
-		return "Hello, world!"
-		
-if __name__ = "__main__":
-	app = web.application(urls, globals())
-	app.run()
+    def GET(self):
+        return "Hello, world!"
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
